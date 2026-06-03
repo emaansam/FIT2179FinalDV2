3696c8a32fe30e3972a876cf13096f51db6af7a1
emaansam <emaanasamdani@gmail.com>
2026-06-03 22:48:05 +1000

    Add files via upload

---
+++ DV2/dv2fidioms.js
@@ -0,0 +1,40 @@
+var vg_1 = "dumbell.vl.json";
+vegaEmbed("#dumbell_chart", vg_1).then(function(result) {
+}).catch(console.error);
+
+var vg_2 = "rankchange.vl.json";
+vegaEmbed("#rankings", vg_2).then(function(result) {
+}).catch(console.error);
+
+var vg_3 = "schooltype.vl.json";
+vegaEmbed("#donut", vg_3).then(function(result) {
+}).catch(console.error);
+
+var vg_4 = "cohortsize.vl.json";
+vegaEmbed("#bubble", vg_4).then(function(result) {
+}).catch(console.error);
+
+
+var vg_5 = "map.vl.json";
+vegaEmbed("#map", vg_5).then(function(result) {
+}).catch(console.error);
+
+var vg_6 = "rising.vl.json";
+vegaEmbed("#slope", vg_6).then(function(result) {
+}).catch(console.error);
+
+var vg_7 = "selective.vl.json";
+vegaEmbed("#bar", vg_7).then(function(result) {
+}).catch(console.error);
+
+var vg_8 = "gender.vl.json";
+vegaEmbed("#gender", vg_8).then(function(result) {
+}).catch(console.error);
+
+var vg_9 = "ib.vl.json";
+vegaEmbed("#ib", vg_9).then(function(result) {
+}).catch(console.error);
+
+var vg_10 = "maxatar.vl.json";
+vegaEmbed("#scatter", vg_10).then(function(result){
+}).catch(console.error);
